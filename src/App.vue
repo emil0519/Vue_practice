4b896b84980865689737f6cd4cabc3bdf8067cc9
Emil Lau <r09343018@ntu.edu.tw>
2024-09-30 13:13:57 +0800

    feat:Header UI

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
 import { useFetchData } from "./assets/composables/useFetchData";
 import { SortEnum } from "./type";
+import Header from './components/Header.vue'; // Adjust the path as necessary
+
 
 const { resultList, isLoading, isError } = useFetchData({ sortOption: SortEnum.Total_Time });
 
 </script>
 
 <template>
-  <header></header>
-
-  <main></main>
+<Header></Header>
+  <main class="container">
+    
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container{
+  padding: 3rem
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="header-section">
+    <h1 class="header-text">Triathlon Result</h1>
+  </header>
+</template>
+
+<style scoped>
+.header-section {
+  position: sticky;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 8%;
+  background-color: var(--dark);
+  display: flex;
+  align-items: center;
+  padding-left: 5px;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  z-index: 2;
+}
+.header-text {
+  font-size: 40px;
+  font-weight: 700;
+  color: white;
+}
+</style>
