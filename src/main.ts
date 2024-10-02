73429d40198ba0a50201d4b8c34c03f23ab35152
Emil Lau <r09343018@ntu.edu.tw>
2024-10-02 23:34:57 +0800

    core:Set up PrimeVue

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,37 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
+import { definePreset } from "@primevue/themes";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: "#f9fafb",
+      100: "#f3f4f6",
+      200: "#e5e7eb",
+      300: "#d1d5db",
+      400: "#9ca3af",
+      500: "#6b7280",
+      600: "#4b5563",
+      700: "#374151",
+      800: "#1f2937",
+      900: "#111827",
+      950: "#030712",
+    },
+  },
+});
+
+app.use(PrimeVue, {
+  theme: {
+    preset: MyPreset,
+    options: {
+      darkModeSelector: "disabled",
+    },
+  },
+});
+
+app.mount("#app");
